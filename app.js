6ac09e4c15f8804cc3d696e8b43848db306baac0
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2022-06-05 19:49:37 +0500

    Class1
    Covered coding of Class1

---
+++ app.js
@@ -0,0 +1,17 @@
+//alert("How are you?");
+var name = "Moinuddin";
+var firstName = "Muhammad";
+//alert(firstName + " " + name);
+var one = 1;
+var two = 2;
+//alert(one + two);
+var num1 = 30;
+var num2 = 40;
+var num3 = 10;
+var num4 = 20;
+var num5 = 
+
+alert(num1 + num2 - one - two) // 70-3 = 67
+alert(num1 * num3 + num2 / num4 - one - two) // 299
+alert(num1 / num3 % num2); // (30 / 10 % 40) --> 3
+alert(num1 / num2 % num3); // (30 / 40 % 10) --> 0.75
